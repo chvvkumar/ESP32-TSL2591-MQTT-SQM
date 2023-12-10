6f7a41d1e5ffdcf6523c57020ae5726ce77959bd
Kumar Challa <chvvkumar@users.noreply.github.com>
2023-12-09 19:14:35 -0600

    Create arduino_secrets.h

---
+++ arduino_secrets.h
@@ -0,0 +1,10 @@
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+#define NETWORK_HOSTNAME ""
+#define MQTT_BROKER ""
+#define MQTT_PORT 1883
+#define MQTT_CLIENT "ESP_SQM"
+#define MQTT_TOPIC "Astro/SQM"
+
+// Interval to update sensor data in milliseconds
+#define MQTT_UPDATE_INTERVAL 60000
