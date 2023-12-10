738dd0c39e60802c7bfed072e87de0cfed381b37
Kumar Challa <chvvkumar@users.noreply.github.com>
2023-12-10 01:14:36 -0600

    Update arduino_secrets.h

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -4,7 +4,7 @@
 #define MQTT_BROKER ""
 #define MQTT_PORT 1883
 #define MQTT_CLIENT "ESP_SQM"
-#define MQTT_TOPIC "Astro/SQM"
+#define MQTT_SQM_JSON_TOPIC "Astro/SQMMeter"
 
 // Interval to update sensor data in milliseconds
 #define MQTT_UPDATE_INTERVAL 60000
